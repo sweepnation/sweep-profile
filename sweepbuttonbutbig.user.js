00add149df9bf7801867b71d498c6f76e9f867ab
sweepnation <beastplayz2010@gmail.com>
2024-09-02 01:33:40 +0100

    Create sweepbuttonbutbig.user.js

---
+++ sweepbuttonbutbig.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         sweephvh Button
+// @description  Script that adds a button that redirects to sweephvh profile
+// @version      1.1
+// @author       sweephvh
+// @match        https://gamesense.pub/forums/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=gamesense.pub
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function addsweepButton() {
+        // Create the new button element
+        var sweepLink = document.createElement("li");
+        sweepLink.id = "navsweep";
+        sweepLink.innerHTML = '<a class="usergroup-1" href="https://gamesense.pub/forums/profile.php?id=15297" id="sweepButton">sweephvh</a>';
+
+        // Insert the new button after the premium element
+        var premiumElement = document.getElementById("navpremium");
+        if (premiumElement) {
+            premiumElement.parentNode.insertBefore(sweepLink, premiumElement.nextSibling);
+        }
+
+        // Apply custom styling to the button text
+        var style = document.createElement('style');
+        style.textContent = `
+            #sweepButton {
+                font-size: 50px;
+                /* Optional: you can add more styling here if needed */
+            }
+        `;
+        document.head.appendChild(style);
+    }
+
+    addsweepButton();
+})();
